f6b3d0fdd43a0d974218dab50602cffa26940fbc
Isaac Obella <wizlif@users.noreply.github.com>
2023-06-20 18:14:14 +0300

    chore: fix formatting
    Signed-off-by: Isaac Obella <wizlif@users.noreply.github.com>

--- test/features/home/repos/weather/weather_repo.mock.dart
+++ test/features/home/repos/weather/weather_repo.mock.dart
@@ -2,4 +2,4 @@ import 'package:mocktail/mocktail.dart';
 import 'package:nssf_interview/features/home/repos/weather/i_weather_repo.dart';
 
 /// Mock weather repo
-class MockWeatherRepo extends Mock implements IWeatherRepo{}
+class MockWeatherRepo extends Mock implements IWeatherRepo {}
